13432fb4fcad414d9cdc8dea8fcabfcd8e3f8b7e
wernich-vg <wernich.bekker@vatglobal.com>
2021-11-11 10:26:24 +0200

    Change image tag to 0.11

--- src/sample/trivial-web-server.go
+++ src/sample/trivial-web-server.go
@@ -6,12 +6,12 @@ import (
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "I am a GO application running inside Docker. - 0.13")
+	fmt.Fprintf(w, "I am a GO application running inside Docker. - 0.14")
 
 }
 
 func main() {
-	fmt.Println("Basic web server is starting on port 8080...")
+	fmt.Println("Basic web server is starting on port 80...")
 	http.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":80", nil)
 }
